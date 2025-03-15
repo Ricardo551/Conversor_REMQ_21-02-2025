7b0ba6178162585a581e4f7c7b7c11681298df70
Ricardo Ernesto Mejia Quitanilla <ia.ricardomejia@ufg.edu.sv>
2025-03-14 18:21:22 -0600

    Version Final

--- sw.js
+++ sw.js
@@ -1,32 +1,33 @@
-const CACHE_NAME = "convertidor-temperatura-v1";
+//sw: Service Worker
+const CACHE_NAME = "Convertidor-temperatura-v1";
 
 self.addEventListener('install',event => {
     event.waitUntil((async()=>{
-        const cache = await caches.open(CACHE_NAME)
+        const cache = await caches.open(CACHE_NAME);
         cache.addAll([
             './',
-            //'./index.html',
-            '/js/converter.js',
-         './css/style.css',
-         ])
+            //'index.html',
+            './js/converter.js',
+            './css/style.css',
+        ]);
     })());
 });
 
-self.addEventListener('fetch',event => {
-    event.repondWith((async () =>{
-        const cache = await caches.open(CACHE_NAME);
+self.addEventListener('fetch',event=> {
+    event.respondWith((async () =>{
+        const cache = await caches.open(CACHE_NAME); //mod
         const cachedResponse = await cache.match(event.request);
 
         if(cachedResponse){
             return cachedResponse;
-         }else{
-             try{
-                 const fetchResponse = await fetch(event.request);
+        }else{
+            try{
+                const fetchResponse = await fetch(event.request);
                 cache.put(event.request,fetchResponse.close());
                 return fetchResponse;
-             }catch(e){
+            }catch(e){
                 //Hubo problemas de red de datos.
-             }
+            }
         }
-    }));        
+    })());
 });
